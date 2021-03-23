5793621a14e1a3dca7e128628a84a5a1f73ec50f
Anton Kesy <anton@kesy.de>
2021-03-23 16:36:36 +0100

    Create new GUI Form

---
+++ src/de/hso/rechenarchitektur/picsimulator/SimulatorGUI.java
@@ -0,0 +1,21 @@
+package de.hso.rechenarchitektur.picsimulator;
+
+import javax.swing.*;
+
+public class SimulatorGUI {
+
+    private JPanel panelMain;
+
+    public SimulatorGUI(){
+        panelMain = new JPanel(); //Overwrites UI Designer
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PIC Simulator");
+        frame.setContentPane(new SimulatorGUI().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}

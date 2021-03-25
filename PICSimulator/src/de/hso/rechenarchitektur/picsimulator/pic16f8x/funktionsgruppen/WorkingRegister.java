64f24f8b4f396a90822e623428bfcdd0cf5c3a76
Anton Kesy <anton@kesy.de>
2021-03-25 12:12:52 +0100

    Create WorkingRegister.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/WorkingRegister.java
@@ -0,0 +1,8 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+/**
+ * Das Working-Register liegt nicht im bekannten RAMAdressbereich. Es ist lediglich ein Hilfsregister, in dem ein
+ * Argument steht, das in der ALU verknüpft werden soll.
+ */
+public class WorkingRegister {
+}

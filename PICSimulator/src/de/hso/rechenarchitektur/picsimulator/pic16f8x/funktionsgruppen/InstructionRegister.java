626359879bf81d9895f30992c084fe155f68af4f
Anton Kesy <anton@kesy.de>
2021-03-25 11:57:32 +0100

    Update InstructionRegister.java

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/InstructionRegister.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/InstructionRegister.java
@@ -1,4 +1,13 @@
 package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
 
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.instructions.Instruction;
+
 public class InstructionRegister {
+
+    private Instruction instruction; //NULL == NOP
+
+
+    public InstructionRegister(){
+
+    }
 }

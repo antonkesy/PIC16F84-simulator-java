f0fd7d60709abf760f3354575a30fe14b39f3d49
Anton Kesy <anton@kesy.de>
2021-03-25 10:52:29 +0100

    Create ProgramMemory.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/ProgramMemory.java
@@ -0,0 +1,9 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+public class ProgramMemory {
+    private int[]memory;
+
+    public ProgramMemory(){
+        memory = new int[1024];
+    }
+}

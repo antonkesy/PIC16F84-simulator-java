b6b53ad11f485431c0350ab6b1334e02dd0b7cc4
Anton Kesy <anton@kesy.de>
2021-03-25 12:12:18 +0100

    Create IOManagement.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/IOManagement.java
@@ -0,0 +1,14 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+/**
+ * Ein 4-Bit Port RA und ein 8-Bit Port RB stellen die
+ * Verbindung zur Außenwelt her. Jede Leitung kann
+ * individuell im TRIS-Register als Ein- oder Ausgang
+ * definiert werden
+ *
+ * Die eigentliche Ausgabe erfolgt durch Beschreiben des
+ * PORT-Registers. Um die Eingangspegel einzulesen,
+ * reicht es aus, dieses PORT-Register zu lesen.
+ */
+public class IOManagement {
+}

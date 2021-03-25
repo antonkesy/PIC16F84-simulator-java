27c290ba0e36f7477a6379feeab5e0026fc89a82
Anton Kesy <anton@kesy.de>
2021-03-25 10:52:23 +0100

    Create Stack.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/Stack.java
@@ -0,0 +1,23 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+public class Stack {
+    private int[]stackArray;
+    private short index;
+
+    public Stack(){
+        stackArray = new int[8]; //Todo("startwerte setzen")
+        index = 0;
+    }
+
+    /**
+     * Fuegt eine neue Adresse in den Stack hinzu und setzt den index auf den naechst hoeren Wert
+     * @param newAddress
+     */
+    public void AddNewAddress(int newAddress){
+        if(++index>stackArray.length-1){
+            index = 0;
+        }
+        stackArray[index] = newAddress;
+    }
+
+}

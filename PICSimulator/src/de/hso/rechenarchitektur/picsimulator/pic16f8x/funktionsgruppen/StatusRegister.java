2fe739b3b7501e32fec3466e6f15945e2b197325
Anton Kesy <anton@kesy.de>
2021-03-25 11:04:58 +0100

    Create StatusRegister.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/StatusRegister.java
@@ -0,0 +1,41 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+public class StatusRegister {
+    private boolean carryFlag, digitCarryFlag, zeroFlag;
+
+    public boolean isCarryFlag() {
+        return carryFlag;
+    }
+
+    public void setCarryFlag(boolean value) {
+        carryFlag = value;
+    }
+
+    public void resetCarryFlag() {
+        carryFlag = false;
+    }
+
+    public boolean isDigitCarryFlag() {
+        return digitCarryFlag;
+    }
+
+    public void setDigitCarryFlag(boolean value) {
+        digitCarryFlag = value;
+    }
+
+    public void resetDigitCarryFlag() {
+        digitCarryFlag = false;
+    }
+
+    public boolean isZeroFlag() {
+        return zeroFlag;
+    }
+
+    public void setZeroFlag(boolean value) {
+        zeroFlag = value;
+    }
+
+    public void resetZeroFlag() {
+        zeroFlag = false;
+    }
+}

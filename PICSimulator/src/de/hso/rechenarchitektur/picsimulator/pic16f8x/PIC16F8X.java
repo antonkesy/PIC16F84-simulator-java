4243286dc94304ff9f742ac801d51892c55d662e
Anton Kesy <anton@kesy.de>
2021-03-25 11:09:00 +0100

    Update PIC16F8X.java

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/PIC16F8X.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/PIC16F8X.java
@@ -1,5 +1,22 @@
 package de.hso.rechenarchitektur.picsimulator.pic16f8x;
 
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen.*;
+
 public class PIC16F8X {
+    private ProgramClock programClock;
+    private ProgramClockLath programClockLath;
+    private ProgramMemory programMemory;
+    private RandomAccessMemory randomAccessMemory;
+    private Stack stack;
+    private StatusRegister statusRegister;
+
+    public PIC16F8X() {
+        programClock = new ProgramClock();
+        programClockLath = new ProgramClockLath();
+        programMemory = new ProgramMemory();
+        randomAccessMemory = new RandomAccessMemory();
+        stack = new Stack();
+        statusRegister = new StatusRegister();
+    }
 
 }

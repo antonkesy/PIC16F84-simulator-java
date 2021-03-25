91f41e57c621e54b65c544957d9c285e499ded88
Anton Kesy <anton@kesy.de>
2021-03-25 13:08:52 +0100

    add calculate opcode

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/instructions/register_operations/bit_oriented/BCF.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/instructions/register_operations/bit_oriented/BCF.java
@@ -4,8 +4,14 @@ import de.hso.rechenarchitektur.picsimulator.pic16f8x.instructions.Instruction;
 
 public class BCF extends Instruction {
     public BCF(int f, int b) {
-        super(f, b);
-        //opcode = 01_00bb_bfff_ffff;
-        affectedStatus = new boolean[]{false, false, false};
+        super(0b01_00,f, b,new boolean[]{false, false, false}); //0b01_00bb_bfff_ffff
+    }
+
+    @Override
+    protected void calculateOPCode() {
+        opcode <<=3; //fester opcode wird erweitert um f
+        opcode^=dB; // or mit f
+        opcode <<=7; //opcode um d erweitert
+        opcode^=fK;
     }
 }

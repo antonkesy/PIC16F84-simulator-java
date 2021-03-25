ae9ae1789c980d5fd682d604fb5d5d142c6980bb
Anton Kesy <anton@kesy.de>
2021-03-25 09:23:30 +0100

    Update PIC16F8XTest.java

--- src/de/hso/rechenarchitektur/picsimulator/test/junit/PIC16F8XTest.java
+++ src/de/hso/rechenarchitektur/picsimulator/test/junit/PIC16F8XTest.java
@@ -1,10 +1,14 @@
 package de.hso.rechenarchitektur.picsimulator.test.junit;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.PIC16F8X;
+import org.junit.Test;
 
 public class PIC16F8XTest {
 
-    
+    @Test
+    public void testIsEmpty() {
+        PIC16F8X testPic = new PIC16F8X();
+        //TODO("create test for functionality")
+    }
 
 }

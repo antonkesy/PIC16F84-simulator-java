be09ba43f03a6646071c6425e220029ac4ddc137
Anton Kesy <anton@kesy.de>
2021-03-25 10:58:57 +0100

    Create RandomAccessMemory.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/RandomAccessMemory.java
@@ -0,0 +1,31 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+
+public class RandomAccessMemory {
+    /**
+     * Bank0 = [0-127][0] --- Bank1 = [0-127][1]
+     */
+    private int[][] memory;
+    private boolean isBank0;
+
+    public RandomAccessMemory() {
+        memory = new int[128][2];
+        isBank0 = true;
+    }
+
+    public int getDataFromAddress(int address) {
+        return memory[address][isBank0 ? 0 : 1];
+    }
+
+    public void setDataToAddress(int address, int data) {
+        memory[address][isBank0 ? 0 : 1] = data;
+    }
+
+    public void SetBank0() {
+        isBank0 = true;
+    }
+
+    public void SetBank1() {
+        isBank0 = false;
+    }
+}

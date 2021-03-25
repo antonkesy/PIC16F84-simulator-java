38116248f058763cbce2b3af575c05b439431d36
Anton Kesy <anton@kesy.de>
2021-03-25 13:09:07 +0100

    Update Instruction.java

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/instructions/Instruction.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/instructions/Instruction.java
@@ -1,19 +1,37 @@
 package de.hso.rechenarchitektur.picsimulator.pic16f8x.instructions;
 
+/**
+ * p56 PIC16F8X Doku
+ */
 public abstract class Instruction {
-    protected short cycles;
+    private short cycles;
     protected int opcode;
     protected int fK;
     protected int dB; //d-b-k
-    protected boolean[] affectedStatus; //c, dz,z -> true == betroffen
+    private boolean[] affectedStatus; //c, dz,z -> true == betroffen
 
-    public Instruction(int fK) {
+    public Instruction(int opcode, int fK, boolean[] affectedStatus) {
+        this.opcode = opcode;
         this.fK = fK;
+        this.affectedStatus = affectedStatus;
+        calculateOPCode();
     }
 
-    public Instruction(int fK, int dB) {
+    public Instruction(int opcode,int fK, int dB, boolean[] affectedStatus) {
+        this.opcode = opcode;
         this.fK = fK;
         this.dB = dB;
+        this.affectedStatus = affectedStatus;
+        calculateOPCode();
+    }
+
+    /**
+     * Errechnet den OPCode/Bitmaske mit fk,db
+     */
+    protected abstract void calculateOPCode();
+
+    public int getOpcode(){
+        return opcode;
     }
 
 }

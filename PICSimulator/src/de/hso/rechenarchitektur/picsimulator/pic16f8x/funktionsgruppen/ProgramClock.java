d072f763ee2e36c0c902b74859c2cb3cf16ca827
Anton Kesy <anton@kesy.de>
2021-03-25 11:01:16 +0100

    Create ProgramClock.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/ProgramClock.java
@@ -0,0 +1,17 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+public class ProgramClock {
+    private int clock;
+
+    public ProgramClock() {
+        clock = 0;
+    }
+
+    public void setProgramClock(int value) {
+        clock = value;
+    }
+
+    public int getClock() {
+        return clock;
+    }
+}

56b5d2edd79289a7451dadfc8eb702bc374d87c4
Anton Kesy <anton@kesy.de>
2021-03-25 12:13:04 +0100

    Description

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/ArithmeticLogicUnit.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/ArithmeticLogicUnit.java
@@ -1,4 +1,10 @@
 package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
 
+/**
+ * Die ALU verknüpft zwei Argumente. Die Verknüpfungsarten
+ * sind hier: ADD, SUB, AND, OR, XOR und NOT (1-er
+ * Komplement).
+ * In Abhängigkeit vom Ergebnis werden Flags gesetzt.
+ */
 public class ArithmeticLogicUnit {
 }

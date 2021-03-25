e4c25c08655398e2788c7dbb18c5b6d817c2fcbe
Anton Kesy <anton@kesy.de>
2021-03-25 12:12:23 +0100

    Create FileSelectRegister.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/funktionsgruppen/FileSelectRegister.java
@@ -0,0 +1,10 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x.funktionsgruppen;
+
+/**
+ * Dieses Register enthält den Zeiger (Fileadresse) wenn
+ * eine indirekte Adressierung gemacht werden soll. Beim
+ * Zugriff auf Fileadresse 0 (IND) wird diese 0 durch den
+ * Inhalt des FSR ersetzt.
+ */
+public class FileSelectRegister {
+}

6739746e6ff919ffaff33947b4a3b1ca3aac968c
Anton Kesy <anton@kesy.de>
2021-03-27 20:12:26 +0100

    getIntegerFromCode + Instruction

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/parser/FileReader.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/parser/FileReader.java
@@ -10,10 +10,10 @@ import java.util.*;
 public class FileReader {
     private final File file;
 
-    Map<Integer, Instruction> instructionList;
+    Map<Integer, Instruction> instructionMap;
 
     ArrayList<String> lines;
-    Map<String, Integer> ownSymbols;
+    Map<String, Integer> ownSymbolsMap;
     Map<String, Integer> jumpMap;
 
     int nextProgramMemoryPosition = 0;
@@ -21,18 +21,24 @@ public class FileReader {
     public FileReader(File filePath) {
         this.file = filePath;
         lines = new ArrayList<>();
-        ownSymbols = new HashMap<>();
+        ownSymbolsMap = new HashMap<>();
         jumpMap = new HashMap<>();
+        instructionMap = new HashMap<>();
         readFile();
 
         System.out.println("\nownSymbols:");
-        for (Map.Entry<String, Integer> entry : ownSymbols.entrySet()) {
+        for (Map.Entry<String, Integer> entry : ownSymbolsMap.entrySet()) {
             System.out.println(entry.getKey() + "\t" + entry.getValue());
         }
 
         System.out.println("\nJumper:");
         for (Map.Entry<String, Integer> entry : jumpMap.entrySet()) {
             System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+
+        System.out.println("\nInstructions:");
+        for (Map.Entry<Integer, Instruction> entry : instructionMap.entrySet()) {
+            System.out.println(entry.getKey() + "\t" + entry.getValue().toString());
         }
     }
 
@@ -80,32 +86,64 @@ public class FileReader {
             case 4:
                 //Symbole
                 if (lineSplit[2].equalsIgnoreCase("equ")) {
-                    int value;
-                    //Hex Indikator entfernen
-                    if (lineSplit[3].toLowerCase().endsWith("h")) {
-                        value = Integer.decode("0x" + lineSplit[3].substring(0, lineSplit[3].length() - 1));
-                    } else if (lineSplit[3].toLowerCase().endsWith("b")) {
-                        value = Integer.parseInt(lineSplit[3].substring(0, lineSplit[3].length() - 1), 2);
-                    } else {
-                        //TODO Oct case
-                        value = Integer.parseInt(lineSplit[3]);
-                    }
-                    ownSymbols.put(lineSplit[1], value);
+                    int value = getIntegerValueFromCode(lineSplit[3]);
+                    ownSymbolsMap.put(lineSplit[1], value);
                     break;
                 }
                 //non param Instruktions
-                System.out.println("Instruction\t" + lineSplit[0] + "\t" + lineSplit[1] + "\t");
+                int programMemoryPosition = getIntegerValueFromCode(lineSplit[0]);
+                nextProgramMemoryPosition = programMemoryPosition + 1;
+                instructionMap.put(programMemoryPosition, new Instruction(getInstructionType(lineSplit[3])));
                 //TODO Hex to Instruction
                 break;
             case 5:
                 //Instruktions + param
-                System.out.println("Instruction\t" + lineSplit[0] + "\t" + lineSplit[1] + "\t");
+                programMemoryPosition = Integer.decode("0x" + lineSplit[0]);
+                nextProgramMemoryPosition = programMemoryPosition + 1;
+                if (lineSplit[3].equalsIgnoreCase("goto")) {
+                    System.out.println("GoTo " + lineSplit[4]); //TODO
+                } else if (lineSplit[4].contains(",")) {
+                    String[] values = lineSplit[4].split(",");
+                    instructionMap.put(programMemoryPosition,
+                            new Instruction(getInstructionType(lineSplit[3]),
+                                    getIntegerValueFromCode(values[0]),
+                                    getIntegerValueFromCode(values[1])));
+                } else {
+                    instructionMap.put(programMemoryPosition, new Instruction(getInstructionType(lineSplit[3]), getIntegerValueFromCode(lineSplit[4])));
+                }
                 break;
         }
     }
 
     public List<String> getLineList() {
         return lines;
+    }
+
+    private int getIntegerValueFromCode(String code) {
+        if (ownSymbolsMap.containsKey(code)) {
+            return ownSymbolsMap.get(code);
+        }
+        if (code.toLowerCase().endsWith("h")) {
+            return Integer.decode("0x" + code.substring(0, code.length() - 1));
+        } else if (code.toLowerCase().endsWith("b")) {
+            return Integer.parseInt(code.substring(0, code.length() - 1), 2);
+        } else if (code.equalsIgnoreCase("w")) {
+            //MOVF d Flag
+            return 0;
+        } else if (code.equalsIgnoreCase("f")) {
+            //MOVF d Flag
+            return 1;
+        }
+        return Integer.parseInt(code);
+    }
+
+    private InstructionType getInstructionType(String typeAsString) {
+        for (InstructionType instructionType : InstructionType.values()) {
+            if (instructionType.code.equals(typeAsString)) {
+                return instructionType;
+            }
+        }
+        return InstructionType.NOP;
     }
 }
 

27f620306f4e1030147c5a2b1dc941fa32cb8d56
Anton Kesy <anton@kesy.de>
2021-03-27 11:38:48 +0100

    GUI Updates
    ...

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/SimulatorGUI.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/SimulatorGUI.java
@@ -5,10 +5,43 @@ import javax.swing.*;
 public class SimulatorGUI {
 
     private JPanel panelMain;
+    private JTable table1;
+    private JButton resetButton;
+    private JButton startButton;
+    private JButton stepButton;
+    private JButton stoppButton;
+    private JComboBox comboBox1;
+    private JCheckBox a4CheckBox;
+    private JCheckBox a0CheckBox;
+    private JCheckBox a3CheckBox1;
+    private JCheckBox a3CheckBox;
+    private JCheckBox a1CheckBox;
+    private JCheckBox a7CheckBox;
+    private JCheckBox a6CheckBox;
+    private JCheckBox a0CheckBox1;
+    private JCheckBox a1CheckBox1;
+    private JCheckBox a2CheckBox;
+    private JCheckBox a3CheckBox2;
+    private JCheckBox a4CheckBox1;
+    private JCheckBox a5CheckBox;
+    private JCheckBox a4CheckBox2;
+    private JCheckBox a0CheckBox2;
+    private JCheckBox a1CheckBox2;
+    private JCheckBox a2CheckBox1;
+    private JCheckBox a3CheckBox3;
+    private JCheckBox a7CheckBox1;
+    private JCheckBox a6CheckBox1;
+    private JCheckBox a0CheckBox3;
+    private JCheckBox a1CheckBox3;
+    private JCheckBox a2CheckBox2;
+    private JCheckBox a3CheckBox4;
+    private JCheckBox a5CheckBox1;
+    private JCheckBox a4CheckBox3;
+    private JCheckBox a5CheckBox2;
+    private JCheckBox a6CheckBox2;
+    private JCheckBox a7CheckBox2;
+    private JList list1;
 
-    public SimulatorGUI(){
-        panelMain = new JPanel(); //Overwrites UI Designer
-    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("PIC Simulator");
@@ -18,4 +51,7 @@ public class SimulatorGUI {
         frame.setVisible(true);
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }

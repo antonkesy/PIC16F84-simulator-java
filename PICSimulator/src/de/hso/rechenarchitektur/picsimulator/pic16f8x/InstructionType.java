0ccf52ea1fcf8d7c620e9297a7438ea0481af6d0
Anton Kesy <anton@kesy.de>
2021-03-27 19:41:14 +0100

    Add InstructionType Code Command

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/InstructionType.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/InstructionType.java
@@ -1,7 +1,16 @@
 package de.hso.rechenarchitektur.picsimulator.pic16f8x;
 
 public enum InstructionType {
-    ADDWF, ANDWF, CLRF, CLRW, COMF, DECF, DECFSZ, INCF, INCFSZ, IORWF, MOVF, MOVWF, NOP, RLF, RRF, SUBWF,
-    SWAPF, XORWF, BCF, BSF, BTFSC, BTFSS, ADDLW, ANDLW, CALL, CLRWDT, GOTO, IORLW, MOVLW, RETFIE, RETLW,
-    RETURN, SLEEP, SUBLW, XORLW
+    ADDWF("addwf"), ANDWF("andwf"), CLRF("clrf"), CLRW("clrw"), COMF("comf"), DECF("decf"), DECFSZ("decfsz"),
+    INCF("incf"), INCFSZ("incfsz"), IORWF("iorwf"), MOVF("movf"), MOVWF("movwf"), NOP("nop"), RLF("rlf"), RRF("rrf"),
+    SUBWF("subwf"), SWAPF("swapf"), XORWF("xorwf"), BCF("bcf"), BSF("bsf"), BTFSC("btfsc"), BTFSS("btfss"), ADDLW("addlw"),
+    ANDLW("andlw"), CALL("call"), CLRWDT("clrwdt"), GOTO("goto"), IORLW("iorlw"), MOVLW("movlw"), RETFIE("retfie"),
+    RETLW("retlw"), RETURN("return"), SLEEP("sleep"), SUBLW("sublw"), XORLW("xorlw");
+
+    public final String code;
+
+    private InstructionType(String code) {
+        this.code = code;
+    }
+
 }

68f1b597380ed9f0d92c0a12124f3d16aa02e97d
Anton Kesy <anton@kesy.de>
2021-04-03 14:00:24 +0200

    Update LSTReaderTest.java

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/test/junit/LSTReaderTest.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/test/junit/LSTReaderTest.java
@@ -4,20 +4,20 @@ import de.hso.rechenarchitektur.picsimulator.reader.FileReader;
 import de.hso.rechenarchitektur.picsimulator.pic16f8x.Instruction;
 import de.hso.rechenarchitektur.picsimulator.pic16f8x.InstructionLine;
 import de.hso.rechenarchitektur.picsimulator.pic16f8x.InstructionType;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
 
 public class LSTReaderTest {
     @Test
     public void testLST1() {
         List<InstructionLine> instructionLineList = new ArrayList<>();
         instructionLineList.add(new InstructionLine(18, 0, new Instruction(InstructionType.MOVLW, 0x11)));
-        instructionLineList.add(new InstructionLine(19, 1, new Instruction(InstructionType.ANDLW, 0x30))); //Nicht sicher wegen 1 fuer w
+        instructionLineList.add(new InstructionLine(19, 1, new Instruction(InstructionType.ANDLW, 0x30)));
         instructionLineList.add(new InstructionLine(20, 2, new Instruction(InstructionType.IORLW, 0x0D)));
         instructionLineList.add(new InstructionLine(21, 3, new Instruction(InstructionType.SUBLW, 0x3D)));
         instructionLineList.add(new InstructionLine(22, 4, new Instruction(InstructionType.XORLW, 0x20)));
@@ -26,7 +26,7 @@ public class LSTReaderTest {
 
         FileReader fileReader = new FileReader(new File("test/LST/TestLST1.lst"));
 
-        assertArrayEquals(instructionLineList.toArray(), fileReader.getInstructionLineList().toArray(new InstructionLine[0]));
+        Assert.assertEquals(instructionLineList, fileReader.getInstructionLineList());
     }
 
 }

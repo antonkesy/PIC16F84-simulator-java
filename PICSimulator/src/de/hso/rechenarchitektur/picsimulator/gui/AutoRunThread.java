ea1dfacf0c792ec88a7848e31ef663947dc83979
Anton Kesy <anton@kesy.de>
2021-04-04 20:02:02 +0200

    Create AutoRunThread.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/AutoRunThread.java
@@ -0,0 +1,23 @@
+package de.hso.rechenarchitektur.picsimulator.gui;
+
+public class AutoRunThread extends Thread {
+
+    private final SimulatorGUI simulatorGUI;
+
+    public AutoRunThread(SimulatorGUI simulatorGUI) {
+        this.simulatorGUI = simulatorGUI;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                simulatorGUI.step();
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

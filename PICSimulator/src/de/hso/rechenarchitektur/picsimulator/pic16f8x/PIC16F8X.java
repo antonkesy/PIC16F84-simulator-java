2aa78dc81c3155b98771eb21b7afe78dc805af5b
Anton Kesy <anton@kesy.de>
2021-04-05 18:53:26 +0200

    reset flags in ALU

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/PIC16F8X.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/PIC16F8X.java
@@ -179,11 +179,28 @@ public class PIC16F8X {
     private int alu(AluOperations operation, int otherValue) {
         //todo check for flags
         int result = wRegister;
+
+        //Zero flag always reset
+        ram.setZeroFlag(false);
+
         switch (operation) {
             case ADD:
+                //Reset affected flags
+                ram.setCarryFlag(false);
+                ram.setDigitCarryFlag(false);
+                //
+                //TODO set flags
                 result += otherValue;
                 break;
             case SUB:
+                //Reset affected flags
+                ram.setCarryFlag(false);
+                ram.setDigitCarryFlag(false);
+                //
+                //Maskenfehler des PIC ->  Ist der Subtrahend kleiner oder gleich dem Minuend, wird das Carryflag gesetzt
+                if (otherValue <= wRegister) {
+                    ram.setCarryFlag(true);
+                }
                 result -= otherValue;
                 break;
             case AND:

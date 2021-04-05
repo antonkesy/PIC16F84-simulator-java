cdce42eb0db74be27517421c758fd95fc8f92c2d
Anton Kesy <anton@kesy.de>
2021-04-05 17:02:20 +0200

    More Register Helper

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/RandomAccessMemory.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/RandomAccessMemory.java
@@ -28,6 +28,11 @@ public class RandomAccessMemory {
     }
 
     public void setDataToAddress(int address, int data) {
+        //Wenn im AnwenderBereich, dann wird es gespiegelt
+        if (address >= 0x0C && address <= 0x2F) {
+            memory[address][isBank0 ? 1 : 0] = data;
+        }
+
         memory[address][getCurrentBankIndex()] = data;
     }
 
@@ -53,6 +58,14 @@ public class RandomAccessMemory {
 
     public int getTMR0() {
         return memory[0][1];
+    }
+
+    public void setIndirect(int value) {
+        setDataToAddress(0, value);
+    }
+
+    public int getIndirect() {
+        return getDataFromAddress(0);
     }
 
     public void setTMR0(int value) {
@@ -81,6 +94,42 @@ public class RandomAccessMemory {
 
     public void setStatus(int value) {
         setDataToAddress(3, value);
+    }
+
+    public void setStatusBits(
+            boolean isCarryFlag,
+            boolean isDigitCarry,
+            boolean isZeroFlag,
+            boolean isPowerDownFlag,
+            boolean isTimeOutFlag,
+            boolean isRegisterBank0) {
+        int statusValue = 0;
+        if (isCarryFlag) {
+            statusValue += 0b1;
+        }
+
+        if (isDigitCarry) {
+            statusValue += 0b10;
+        }
+
+        if (isZeroFlag) {
+            statusValue += 0b100;
+        }
+
+        if (isPowerDownFlag) {
+            statusValue += 0b1000;
+        }
+
+        if (isTimeOutFlag) {
+            statusValue += 0b1_0000;
+        }
+
+        if (isRegisterBank0) {
+            statusValue += 0b10_0000;
+        }
+
+        setStatus(statusValue);
+
     }
 
     public int getFSR() {
@@ -115,13 +164,44 @@ public class RandomAccessMemory {
         memory[6][1] = value;
     }
 
-
     public int getTrisB() {
         return memory[1][6];
     }
 
     public void setTrisB(int value) {
         memory[6][1] = value;
+    }
+
+    public void setEEData(int value) {
+        memory[8][0] = value;
+    }
+
+    public int getEEData() {
+        return memory[8][0];
+    }
+
+    public void setEECon1(int value) {
+        memory[8][1] = value;
+    }
+
+    public int getEECon1() {
+        return memory[8][1];
+    }
+
+    public void setEEAdr(int value) {
+        memory[9][0] = value;
+    }
+
+    public int getEEAdr() {
+        return memory[9][0];
+    }
+
+    public void setEECon2(int value) {
+        memory[9][1] = value;
+    }
+
+    public int getEECon2() {
+        return memory[9][1];
     }
 
     public int getPCLath() {

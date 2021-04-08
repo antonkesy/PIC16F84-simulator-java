54677d3e4883bc4dcfd297f366fb7a025ae90ce9
Anton Kesy <anton@kesy.de>
2021-04-08 09:56:10 +0200

    Create ALUTest.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/test/junit/ALUTest.java
@@ -0,0 +1,27 @@
+package de.hso.rechenarchitektur.picsimulator.test.junit;
+
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.ArithmeticLogicUnit;
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.RandomAccessMemory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ALUTest {
+    public RandomAccessMemory ram;
+
+    @Before
+    public void setUpTests() {
+        ram = new RandomAccessMemory();
+    }
+
+    @Test
+    public void testADD() {
+        Assert.assertEquals(0x25, ArithmeticLogicUnit.add(ram, 0, 0x25));
+        //TODO check status flags
+        Assert.assertEquals(0x25, ArithmeticLogicUnit.add(ram, 0x25, 0));
+        //TODO check status flags
+        Assert.assertEquals(0xFF, ArithmeticLogicUnit.add(ram, 0xF0, 0xF));
+        //TODO check status flags
+        Assert.assertEquals(0x0, ArithmeticLogicUnit.add(ram, 0xF1, 0xF));
+    }
+}

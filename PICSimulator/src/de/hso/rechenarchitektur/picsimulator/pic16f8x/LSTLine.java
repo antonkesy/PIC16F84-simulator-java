b4443960367c1b782c4935f2e5ba5e58f69cef1e
Anton Kesy <anton@kesy.de>
2021-04-15 08:29:46 +0200

    Create LSTLine.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/pic16f8x/LSTLine.java
@@ -0,0 +1,31 @@
+package de.hso.rechenarchitektur.picsimulator.pic16f8x;
+
+/**
+ * LSTLine Data Class for setting breakpoints
+ */
+public class LSTLine {
+    private boolean isBreakpoint;
+    private final String line;
+
+    public LSTLine(String value) {
+        this.line = value;
+        this.isBreakpoint = false;
+    }
+
+    public boolean isBreakpoint() {
+        return isBreakpoint;
+    }
+
+    public void setBreakpoint(boolean breakpoint) {
+        isBreakpoint = breakpoint;
+    }
+
+    public String getLine() {
+        return isBreakpoint ? "\uD83D\uDD34" : "" + line;
+    }
+
+    @Override
+    public String toString() {
+        return getLine();
+    }
+}

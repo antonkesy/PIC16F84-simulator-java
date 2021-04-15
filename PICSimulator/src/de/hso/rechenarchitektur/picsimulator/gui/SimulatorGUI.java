a9a7ae8f56860b98a4074ae39252a22eac933a97
Anton Kesy <anton@kesy.de>
2021-04-15 08:06:56 +0200

    Fix typo

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/SimulatorGUI.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/SimulatorGUI.java
@@ -297,7 +297,7 @@ public class SimulatorGUI {
     private void createUIComponents() {
         //JTable + Model for FLR
         String[][] fileRegisterData = new String[][]{{"", "", "", "", "", "", "", "", ""}};
-        String[] column = new String[]{"0", "+1", "+2", "+3", "+4", "+6", "+7", "+8"};
+        String[] column = new String[]{"0", "+1", "+2", "+3", "+4", "+5", "+6", "+7"};
         ListModel lm = new FileRegisterTable.RowHeaderListModel();
         modelFileRegister = new DefaultTableModel(fileRegisterData, column);
         fileRegisterTable = new JTable(modelFileRegister);

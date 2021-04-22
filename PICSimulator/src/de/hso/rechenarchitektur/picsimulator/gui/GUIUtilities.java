c1fd1e05f95781fa908da7b86f6f77bcc35453bc
Anton Kesy <anton@kesy.de>
2021-04-22 11:45:37 +0200

    Create GUIUtilities.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/GUIUtilities.java
@@ -0,0 +1,12 @@
+package de.hso.rechenarchitektur.picsimulator.gui;
+
+import javax.swing.table.DefaultTableModel;
+
+public final class GUIUtilities {
+
+    public static void fillModelRowWithData(DefaultTableModel model, String[] data, int row) {
+        for (int i = 0; i < data.length; ++i) {
+            model.setValueAt(data[i], row, i);
+        }
+    }
+}

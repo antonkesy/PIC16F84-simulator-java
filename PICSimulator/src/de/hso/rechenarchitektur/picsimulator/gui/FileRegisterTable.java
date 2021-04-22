bef75933245134a31dc63e8d1db49a7156012069
Anton Kesy <anton@kesy.de>
2021-04-22 11:45:41 +0200

    Update FileRegisterTable.java

--- PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/FileRegisterTable.java
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/gui/FileRegisterTable.java
@@ -1,10 +1,67 @@
 package de.hso.rechenarchitektur.picsimulator.gui;
 
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.PIC16F8X;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
-public class FileRegisterTable {
+import static de.hso.rechenarchitektur.picsimulator.gui.GUIUtilities.fillModelRowWithData;
+
+public class FileRegisterTable extends JTable implements FocusListener {
+    DefaultTableModel modelFileRegisterBank;
+    private PIC16F8X pic;
+    private final boolean isBank0;
+
+    public FileRegisterTable(PIC16F8X pic, JScrollPane frScrollPanel, boolean isBank0) {
+        this.isBank0 = isBank0;
+        this.pic = pic;
+        addFocusListener(this);
+
+        String[][] fileRegisterData = new String[][]{{"", "", "", "", "", "", "", "", ""}};
+        String[] column = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+        ListModel<String> lm = new FileRegisterTable.RowHeaderListModel();
+        modelFileRegisterBank = new DefaultTableModel(fileRegisterData, column);
+        setModel(modelFileRegisterBank);
+        JList<String> rowHeader = new JList<String>(lm);
+        rowHeader.setCellRenderer(new FileRegisterTable.RowHeaderRenderer(this));
+        frScrollPanel.setRowHeaderView(rowHeader);
+
+        //Creates empty rows for FLR Table
+        for (int i = 0; i < 7; ++i) {
+            modelFileRegisterBank.addRow(new String[][]{{"e", "m", "p", "t", "y", "", "", "", ""}});
+        }
+
+    }
+
+    public void updateTable() {
+        if (pic == null) return;
+        String[][] dataArray = pic.getRam().getDataString(isBank0);
+        for (int i = 0; i < dataArray.length; ++i) {
+            fillModelRowWithData(modelFileRegisterBank, dataArray[i], i);
+        }
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (pic == null) return;
+        int inputNumber = 0;
+        try {
+            inputNumber = Integer.decode("0x" + getValueAt(getSelectedRow(), getSelectedColumn()));
+            inputNumber &= 0xFF; //Max 8 Bit
+        } catch (Exception ignored) {
+        }
+        pic.getRam().setDataToAddress((getSelectedColumn()) + (getSelectedRow() * 16), inputNumber);
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        //nothing
+    }
+
 
     static class RowHeaderListModel extends AbstractListModel<String> {
         String[] strRowHeaders;
@@ -40,5 +97,9 @@ public class FileRegisterTable {
             setText((value == null) ? "" : value.toString());
             return this;
         }
+    }
+
+    public void setPIC(PIC16F8X pic) {
+        this.pic = pic;
     }
 }

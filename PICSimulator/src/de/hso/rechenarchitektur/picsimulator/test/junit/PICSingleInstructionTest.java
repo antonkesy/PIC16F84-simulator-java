85e850cb5a35a972c502878f5bdd23ad75b35795
Anton Kesy <anton@kesy.de>
2021-04-23 17:57:03 +0200

    Create PICSingleInstructionTest.java

---
+++ PICSimulator/src/de/hso/rechenarchitektur/picsimulator/test/junit/PICSingleInstructionTest.java
@@ -0,0 +1,41 @@
+package de.hso.rechenarchitektur.picsimulator.test.junit;
+
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.Instruction;
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.InstructionLine;
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.InstructionType;
+import de.hso.rechenarchitektur.picsimulator.pic16f8x.PIC16F8X;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PICSingleInstructionTest {
+
+    PIC16F8X pic;
+
+    public void testBitSetFValues(int testValue, int bitPosition, int expectedValue) {
+        //possible values
+        List<InstructionLine> instructions = new ArrayList<>();
+        instructions.add(new InstructionLine(0, 0, new Instruction(InstructionType.MOVLW, testValue)));
+        instructions.add(new InstructionLine(0, 0, new Instruction(InstructionType.MOVWF, 0x20)));
+        instructions.add(new InstructionLine(0, 0, new Instruction(InstructionType.BSF, 0x20, bitPosition)));
+        instructions.add(new InstructionLine(0, 0, new Instruction(InstructionType.NOP)));
+        pic = new PIC16F8X(instructions);
+        Assert.assertEquals(0, pic.getRam().getDataFromAddress(0x20));
+        pic.step(); //NOP
+        pic.step(); //MOVLW
+        Assert.assertEquals(testValue, pic.getWRegister());
+        pic.step(); //MOVWF
+        Assert.assertEquals(testValue, pic.getRam().getDataFromAddress(0x20));
+        pic.step(); //BSF
+        Assert.assertEquals(expectedValue, pic.getRam().getDataFromAddress(0x20));
+        ////NOP
+    }
+
+    @Test
+    public void testBitSetF() {
+        testBitSetFValues(0xA, 7, 0x8A);
+        testBitSetFValues(0x8A, 7, 0x8A);
+    }
+}
